b18e54a42a41f2ce48e9fff7a3d1f0810097a6a9
冯泽宇 <767228834@qq.com>
2021-01-12 12:56:42 +0800

    冯泽宇的第2次提交，添加登录页面、注册页面

---
+++ pages/logs/logs.js
@@ -0,0 +1,152 @@
+//logs.js
+const util = require('../../utils/util.js')
+const app=getApp()
+Page({
+  data: {
+    account: "",
+    password:"",
+  },
+
+  accountchange:function(e){
+     this.setData({
+       account:e.detail.value.replace(/(^\s*)|(\s*$)/g,'')
+     })
+  },
+
+  passwordchange:function(e){
+    this.setData({
+     password:e.detail.value
+      })
+  },
+
+  formSubmit:function(e){
+    if(this.data.account==''){
+      wx.showToast({
+        title: '未输入账号',
+        icon: 'none'
+      })
+    }
+    else if(this.data.password==''){
+      wx.showToast({
+        title: '未输入密码',
+        icon: 'none'
+      })
+    }
+    else{
+      var curid=this.data.account
+      var curpass=this.data.password
+      const db=wx.cloud.database()
+      const cont = db.collection('account_information')
+      cont.where({
+        id: curid,
+        password:curpass
+      }).get().then(res=>{
+          if(res.data.length==0){
+            wx.showToast({
+              title: '账号或密码错误',
+              icon:'none'
+            })
+          }
+          else{
+            const curidentity=res.data[0].identity
+            const curname=res.data[0].id
+            app.globalData.username=curname
+            app.globalData.identity=curidentity
+            app.globalData.time=res.data[0].time
+            console.log(res.data[0])
+            console.log(app.globalData.username)
+            wx.showToast({
+              title: '登录成功',
+            })
+            setTimeout(function(){
+              wx.switchTab({
+                url: '../index/index'
+              })
+            },2000)
+          }
+      }).catch(res=>{
+        wx.showToast({
+          title: '请检查网络',
+          icon:'none'
+        })
+      })
+    }
+  },
+  formReset:function(){
+     wx.navigateTo({
+       url: '/pages/register/register',
+     })
+  },
+
+  to_register_teacher:function(){
+      wx.navigateTo({
+        url: '/pages/register_teacher/register_teacher',
+      })
+
+  },
+
+  utc_beijing(utc_datetime) {
+    var util = require('../../utils/util.js')
+    // 转为正常的时间格式 年-月-日 时:分:秒
+    var T_pos = utc_datetime.indexOf('T');
+    var Z_pos = utc_datetime.indexOf('Z');
+    var year_month_day = utc_datetime.substr(0, T_pos);
+    var hour_minute_second = utc_datetime.substr(T_pos + 1, Z_pos - T_pos - 1);
+    var new_datetime = year_month_day + " " + hour_minute_second; // 2017-03-31 08:02:06
+
+    // 处理成为时间戳
+    timestamp = new Date(Date.parse(new_datetime));
+    timestamp = timestamp.getTime();
+    timestamp = timestamp / 1000;
+
+    // 增加8个小时，北京时间比utc时间多八个时区
+    var timestamp = timestamp + 8 * 60 * 60;
+
+    // 时间戳转为时间
+   // var beijing_datetime = new Date(parseInt(timestamp) * 1000).toLocaleString().replace(/年|月/g, "-").replace(/日/g, " ");
+    var beijing_datetime = new Date(parseInt(timestamp) * 1000)
+    var dayTime = util.formatTime(beijing_datetime)
+    console.log(dayTime)
+    return beijing_datetime; // 2017-03-31 16:02:06
+  },
+
+  onLoad: function () {
+     wx.cloud.callFunction({
+       name:'getTime',
+     success:function(res){
+       var utc_datetime=res.result
+      var util = require('../../utils/util.js')
+      // 转为正常的时间格式 年-月-日 时:分:秒
+      var T_pos = utc_datetime.indexOf('T');
+      var Z_pos = utc_datetime.indexOf('Z');
+      var year_month_day = utc_datetime.substr(0, T_pos);
+      var hour_minute_second = utc_datetime.substr(T_pos + 1, Z_pos - T_pos - 1);
+      var new_datetime = year_month_day + " " + hour_minute_second; // 2017-03-31 08:02:06
+  
+      // 处理成为时间戳
+      timestamp = new Date(Date.parse(new_datetime));
+      timestamp = timestamp.getTime();
+      timestamp = timestamp / 1000;
+  
+      // 增加8个小时，北京时间比utc时间多八个时区
+      var timestamp = timestamp + 8 * 60 * 60;
+  
+      // 时间戳转为时间
+     // var beijing_datetime = new Date(parseInt(timestamp) * 1000).toLocaleString().replace(/年|月/g, "-").replace(/日/g, " ");
+      var beijing_datetime = new Date(parseInt(timestamp) * 1000)
+      var dayTime = util.formatTime(beijing_datetime)
+      const nowtime=dayTime.split(" ")[0].split("/")
+      const nowsecond=dayTime.split(" ")[1]
+      const nowdate=[nowtime[0],nowtime[1],nowtime[2]]
+      app.globalData.time_standard=dayTime
+      dayTime=dayTime.replace(/\//g,"")
+      dayTime=dayTime.replace(/:/g,'')
+      dayTime=dayTime.replace(/\s/g,"")
+      app.globalData.time_rough=nowdate.join('-')
+      app.globalData.time_standard2=nowdate.join('-')+' '+nowsecond
+      app.globalData.time_str=dayTime
+     }
+  })   
+ }
+  
+})
---
+++ pages/register/register.js
@@ -0,0 +1,235 @@
+// pages/register/register.js
+Page({
+
+  data: {
+          account:"",
+          password:"",
+          passwordagain:"",
+          phonenumber:"",
+          kind:"",
+          kindtrue:false,
+          code:''
+  },
+   
+  invitationchange:function(e){
+       this.setData({
+          code:e.detail.value
+       })
+  },
+
+  changekind:function(e){
+    var temp=false
+    if(e.detail.value=='1'){
+      temp=true
+    }
+    this.setData({
+      kind: e.detail.value,
+      kindtrue: temp
+    })
+  },
+
+  accountchange:function(e){
+    this.setData({
+      account:e.detail.value
+    })
+ },
+
+ passwordchange:function(e){
+   this.setData({
+    password:e.detail.value
+     })
+ },
+
+ passwordagainchange:function(e){
+  this.setData({
+   passwordagain:e.detail.value
+    })
+},
+
+ phonenumberchange:function(e){
+  this.setData({
+   phonenumber:e.detail.value
+    })
+},
+
+ formSubmit:function(e){
+    if(this.data.password==''||this.data.passwordagain==''||this.data.account==''||this.data.phonenumber==''){
+      wx.showToast({
+        title: '有信息未填写',
+        icon:'none'
+      })
+    }
+    else if(this.data.password!=this.data.passwordagain){
+      wx.showToast({
+        title: '两次输入密码不一致',
+        icon:'none'
+      })
+    }
+    else if(this.data.kind==""){
+      wx.showToast({
+        title: '未选择账号类型',
+        icon:'none'
+      })
+    }
+    else if(this.data.kind=="1" && this.data.code==""){
+      wx.showToast({
+        title: '未输入邀请码',
+        icon:'none'
+      })
+    }
+    else{
+      const db=wx.cloud.database()
+      const cont = db.collection('account_information')
+      var curid=this.data.account
+      var curpass=this.data.password
+      var curphone=this.data.phonenumber
+      var curkind=this.data.kind
+      var curcode=this.data.code
+      cont.where({
+        id: curid,
+      }).get().then(res=>{
+        if(res.data.length!=0){
+          wx.showToast({
+            title: '该用户名已注册',
+            icon:'none'
+          })
+        }
+        else{
+         if(curkind=="0")
+          {
+            cont.add({
+            data:{
+              id: curid,
+              password: curpass,
+              phonenumber: curphone,
+              identity:'student'
+            }
+          }).then(res2=>{
+              wx.showToast({
+                title: '注册成功',
+              })
+              setTimeout(function(){
+                wx.navigateBack({
+                  delta:1
+                })},2000)
+            }).catch(res5=>{
+                wx.showToast({
+                  title: '注册失败',
+                  icon:'none'
+              })
+            })
+          }
+            else{
+               const cont2 = db.collection('invite')
+               cont2.get().then(res2=>{
+                    console.log(res2)
+               })
+               console.log(curcode)
+               cont2.where({
+                 code:"1234"
+               }).get().then(res11=>{
+                    console.log(res11)
+                    if(res11.data.length==0){
+                      wx.showToast({
+                        title: '邀请码错误',
+                        icon:'none'
+                      })
+                    }
+                    else{
+                      if(res11.data[0].is_Occupied){
+                        wx.showToast({
+                          title: '激活码被占用',
+                          icon:'none'
+                        })
+                      }
+                      else{
+                        cont2.where({
+                          code:curcode
+                        }).update({
+                          data:{
+                            is_Occupied: true
+                          }
+                        })
+                        cont.add({
+                          data:{
+                            id: curid,
+                            password: curpass,
+                            phonenumber: curphone,
+                            identity:'teacher'
+                          }
+                        }).then(res2=>{
+                            wx.showToast({
+                              title: '注册成功',
+                            })
+                            setTimeout(function(){
+                              wx.navigateBack({
+                                delta:1
+                              })},2000)
+                          })
+                      }
+                    }
+               })
+            }
+        }
+      }).catch(res=>{
+        wx.showToast({
+          title: '注册失败',
+          icon:'none'
+        })
+      })
+    }
+ },
+
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
